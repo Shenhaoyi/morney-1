3bf4eb7052f7cd511bc941054df5909d2e02a7ab
Shenhaoyi <45191012+Shenhaoyi@users.noreply.github.com>
2020-07-09 19:07:46 +0800

    解决了 SVG 自带 fill 属性的问题

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,16 +1,16 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class='selected'>
       <Icon name="money"></Icon>
       记账
     </router-link>
 
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class='selected'>
       <Icon name="labels"></Icon>
       标签
     </router-link>
 
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class='selected'>
       <Icon name="statistics"></Icon>
       统计
     </router-link>
@@ -26,20 +26,25 @@
 <style lang="scss" scoped>
   nav {
     display: flex;
-    box-shadow:0 0 5px rgba(0,0,0,0.25);
-    font-size:12px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.25);
+    font-size: 12px;
+
     > .item {
-      padding:2px 0;
-      width:33.333%;
+      padding: 2px 0;
+      width: 33.333%;
       display: flex;
       flex-direction: column;
       justify-content: center;
       align-items: center;
-      .icon{
-        width:32px;
-        height:32px;
+
+      .icon {
+        width: 32px;
+        height: 32px;
       }
 
+    }
+    > .item.selected {
+      color:blue;
     }
   }
 </style>
