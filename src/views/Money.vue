f568928e6bf51af4ac04e52095d5935996d24256
Shenhaoyi <45191012+Shenhaoyi@users.noreply.github.com>
2020-07-13 22:17:01 +0800

    将 Tabs.vue 使用在 Money.vue 中

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,8 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
-    <types :value.sync="record.type"/>
+    <Tabs :data-source="recordTypeList"
+          :value.sync="record.type"/>
     <div class="notes-wrapper">
       <FormItem field-name="备注："
                 placeholder="在这里输入备注"
@@ -14,19 +15,20 @@
 <script lang="ts">
   import Vue from 'vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
+  import Types from '@/components/Money/Reference.vue';
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import {Component} from 'vue-property-decorator';
+  import Tabs from '@/components/Tabs.vue';
+  import recordTypeList from '@/constants/recordTypeList';
 
   @Component({
-    components: {Tags, FormItem, Types, NumberPad},
-    computed: {
-
-    }
+    components: {Tabs, Tags, FormItem, Types, NumberPad},
+    computed: {}
   })
   export default class Money extends Vue {
     record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
+    recordTypeList = recordTypeList;
 
     onUpdateTags(value: Tag[]) {
       this.record.tags = value; //选中的tags
@@ -43,7 +45,7 @@
     }
 
     saveRecord() {
-      this.$store.commit('createRecord',this.record);
+      this.$store.commit('createRecord', this.record);
     }
   }
 </script>
